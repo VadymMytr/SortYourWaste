4af5203ee4b6871f12628283df6916771aa7037b
VadymMytr <koxdiller@gmail.com>
2023-09-17 11:42:27 +0300

    Add navigation fragment host to the activity, add navigation flow, add required Flow extensions

--- app/src/main/java/vadym/my/wastesorting/presentation/base/BaseVM.kt
+++ app/src/main/java/vadym/my/wastesorting/presentation/base/BaseVM.kt
@@ -1,5 +1,26 @@
 package vadym.my.wastesorting.presentation.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavDirections
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 
-abstract class BaseVM : ViewModel()
+abstract class BaseVM : ViewModel() {
+    val navigationEvent: SharedFlow<NavDirections> = MutableSharedFlow()
+
+    protected fun navigate(directions: NavDirections) {
+        navigationEvent.emitInViewModelScope(directions)
+    }
+
+    protected fun <T> SharedFlow<T>.emitInViewModelScope(value: T) {
+        viewModelScope.launch {
+            emit(value)
+        }
+    }
+
+    private suspend fun <T> SharedFlow<T>.emit(value: T) {
+        (this as? MutableSharedFlow)?.emit(value) ?: throw IllegalStateException("This SharedFlow isn't a MutableSharedFlow.")
+    }
+}
